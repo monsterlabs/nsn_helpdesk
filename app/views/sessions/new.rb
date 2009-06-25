97669d5cd25df1fc183d0473ea2d935f39005f54
Juan Germán Castañeda Echevarría <juanger@gmail.com>
2009-06-25 09:09:03 -0500

    Minor changes. Refactorin view for new session action

--- app/views/sessions/new.rb
+++ app/views/sessions/new.rb
@@ -3,24 +3,19 @@ require "#{RAILS_ROOT}/app/views/layouts/form"
 class Views::Sessions::New < Views::Layouts::Page
   # needs :article
 
-  def content
-    # widget Form, :action => "/session" do
-    #   table = LabelTable.new(:title => "New Comment") do |table|
-    #     table.field("login") do
-    #       textfield :name => "login"
-    #     end
-    #     table.button do
-    #       input :type => "submit", :name => "submit", :value => "Sign In"
-    #     end
-    #   end
-    #   widget table
-    # end
+  def body_content
     form_for @session, :session, :url => session_path do |f|
-      rawtext f.label "Login"
-      rawtext f.text_field :login
-      rawtext f.label "Password"
-      rawtext f.password_field :password
-      rawtext submit_tag "Sign In"
+      p {
+        rawtext f.label "Login"
+        br
+        rawtext f.text_field :login
+      }
+      p {
+        rawtext f.label "Password"
+        br      
+        rawtext f.password_field :password
+      }
+      p { rawtext submit_tag "Sign In" }
     end
   end
   
