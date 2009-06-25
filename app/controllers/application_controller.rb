76a0cfccf52be5c1108c7faf99dee236cc045e96
Juan Germán Castañeda Echevarría <juanger@gmail.com>
2009-06-25 15:57:02 -0500

    Bugfix: Session is now UserSession.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -13,7 +13,7 @@ class ApplicationController < ActionController::Base
   #  private
   def current_user_session
     return @current_user_session if defined?(@current_user_session)
-    @current_user_session = Session.find
+    @current_user_session = UserSession.find
   end
 
   def current_user
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,6 +1,6 @@
 class SessionsController < ApplicationController
-  # before_filter :require_no_user, :only => [:new, :create]
-  # before_filter :require_user, :only => :destroy
+  before_filter :require_no_user, :only => [:new, :create]
+  before_filter :require_user, :only => :destroy
   
   def new
     @session = UserSession.new
