6af45e0fdadf7225c3938174e9f8205dc0577219
Juan Germán Castañeda Echevarría <juanger@gmail.com>
2009-06-29 16:03:04 -0500

    Removing the traditional html layout

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,7 +4,8 @@
 class ApplicationController < ActionController::Base
   helper :all # include all helpers, all the time
   protect_from_forgery # See ActionController::RequestForgeryProtection for details
-
+  layout nil
+  
   # Scrub sensitive parameters from your log
   # filter_parameter_logging :password
   # helper_method :current_user_session, :current_user
