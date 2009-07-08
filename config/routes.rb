dcd1ca6edb1e718034c25c34a3890889416e3165
Juan Germán Castañeda Echevarría <juanger@gmail.com>
2009-07-08 08:42:59 -0500

    Adding failures to routes

--- config/routes.rb
+++ config/routes.rb
@@ -11,6 +11,7 @@ ActionController::Routing::Routes.draw do |map|
     admin.resources :users
     admin.resources :priorities
     admin.resources :regions
+    admin.resources :failures
   end  
 
   map.namespace :operator do |operator|
