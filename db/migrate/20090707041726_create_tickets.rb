e3dfc019f71bd9d0c70dc3ac953f39dc2dd0617f
Alejandro Juarez <karmatr0n.dixit@gmail.com>
2009-07-09 13:30:23 -0600

    Upgrading ticket migration

--- db/migrate/20090707041726_create_tickets.rb
+++ db/migrate/20090707041726_create_tickets.rb
@@ -1,9 +1,14 @@
 class CreateTickets < ActiveRecord::Migration
   def self.up
     create_table :tickets do |t|
-      t.string :title
-      t.references :link
-      t.timestamps
+      t.string     :subject
+      t.text       :body
+      t.references :link # Links:  :frequency_tx, :frequecy_rx
+      t.references :opened_by, :class_name => 'User'
+      t.references :assigned_to, :class_name => 'User'
+      t.references :reported_by, :class_name => 'User'
+      t.references :priority, :status
+      t.timestamps # Opened_date, Resolved_date
     end
   end
 
