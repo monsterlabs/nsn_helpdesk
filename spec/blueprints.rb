e4bcaa01c7dce7e883a9823bcd10e5c934278cae
Alejandro Juarez <karmatr0n.dixit@gmail.com>
2009-07-17 13:41:15 -0600

    Basic blueprints for role, region and status based on forgery

--- spec/blueprints.rb
+++ spec/blueprints.rb
@@ -1,11 +1,23 @@
-Sham.login { InternetForgery.user_name }
-Sham.password { BasicForgery.password }
-Sham.email { InternetForgery.email_address }
+require 'forgery'
+# Temporary Forgery reference: http://github.com/sevenwire/forgery/tree/1c00079ca0bbf74465b3d0b31c74d09e38bea2d2/lib/forgeries
 
 User.blueprint do
-  pwd = Sham.password
-  login
-  email
+  pwd = BasicForgery.password
+  login { InternetForgery.user_name }
+  email { InternetForgery.email_address }
   password { pwd }
   password_confirmation { pwd }
+end
+
+
+Role.blueprint do
+  name { LoremIpsumForgery.word }
+end
+
+Status.blueprint do
+  name { LoremIpsumForgery.word }
+end
+
+Region.blueprint do
+  name { LoremIpsumForgery.word }
 end
