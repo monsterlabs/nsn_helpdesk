fa9fc4f0afb0c41d292f077a5634ade6a1c0a6bf
Fernando Reyes Jiménez <fereyji@gmail.com>
2009-07-21 02:27:01 -0500

    Assign Ticket to Field Manager

--- app/views/field_manager/tickets/actions.rb
+++ app/views/field_manager/tickets/actions.rb
@@ -4,5 +4,7 @@ class Views::FieldManager::Tickets::Actions < Erector::RailsWidget
     link_to 'Edit', :action => 'edit', :id => ticket_id
     text ' | '
     link_to 'Show', :action => 'show', :id => ticket_id
+    text ' | '
+    link_to 'Assign', :action => 'show_field_managers', :id => ticket_id    
   end
 end
---
+++ app/views/field_manager/tickets/show_field_managers.rb
@@ -0,0 +1,22 @@
+class Views::FieldManager::Tickets::ShowFieldManagers < Views::Layouts::Application
+  def page_title
+    "Assign ticket"
+  end
+
+  def view_content
+    rawtext error_messages_for 'ticket', :class => 'ui-state-error ui-corner-all'
+    form_for(@ticket, :url => {:action => "assign_field_manager", :id => @ticket.id}) do |f|
+      h2 do
+        text 'Assign ticket to Field Manager'
+      end
+
+      div :class => 'field_manager_row' do
+        label b "Field Manager:"
+        rawtext f.select :assigned_to_id, User.field_managers.collect { |record| [record.person.fullname, record.id ]}, {:prompt => '-- Select --'}
+      end
+
+      rawtext f.submit 'Assign', ui_style(:button)
+      link_to 'Cancel', {:action => :index},  ui_style(:button)      
+     end
+  end
+end
