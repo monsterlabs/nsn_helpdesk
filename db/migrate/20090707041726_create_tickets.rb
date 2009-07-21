0943749050f26cd0ff0f79fed9280b49b0ec25e5
Alejandro Juarez <karmatr0n.dixit@gmail.com>
2009-07-21 04:14:03 -0600

    Redesigning tickets table

--- db/migrate/20090707041726_create_tickets.rb
+++ db/migrate/20090707041726_create_tickets.rb
@@ -1,23 +1,30 @@
 class CreateTickets < ActiveRecord::Migration
   def self.up
     create_table :tickets do |t|
-      t.string     :ip_address
-      t.string     :subject, :null => false
-      t.text       :body
-      t.references :product
+      # Data provided by customers
+      t.references :reported_by, :class_name => 'User' # The region is an attribute available from user_regions
       t.references :link
-      t.string     :frequency_tx, :frequency_rx
+      t.text       :frequency_tx, :frequency_rx      
+      t.references :product
+      t.references :reported_priority, :null => false, :class_name => 'Priority'
+      t.string     :key # =>  NSNCT.....
+      t.text       :failure_description # Problem description
+      t.text       :alarm_description  # Alarm description
+      t.references :status, :null => false, :default => 1 # Open by default      
+      
+      # Who is opening this ticket?
+      t.string     :ip_address      
       t.references :opened_by, :class_name => 'User'
-      t.references :assigned_to, :class_name => 'User'
-      t.references :reported_by, :class_name => 'User'
-      t.references :attended_by, :class_name => 'User'
-      t.references :region, :class_name => 'Region', :null => false
-      t.references :priority, :null => false
-      t.references :status, :null => false, :default => 1
-      t.references :ticket_type, :null => false
       t.datetime   :opened_at
-      t.string     :key
-      t.timestamps # Opened_date, Resolved_date
+      
+      # Who was taking the call at nokia?
+      t.references :attended_by, :class_name => 'User'
+      t.references :priority, :null => false, :class_name => 'Priority' # Priority suggested by field manager
+      
+      # After ticket registration
+      t.references :assigned_to, :class_name => 'User'
+      t.references :ticket_type, :null => false
+      t.timestamps 
     end
   end
 
