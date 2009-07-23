374db5afea578a98538120b4360c032a01adb402
Juan Germán Castañeda Echevarría <juanger@gmail.com>
2009-07-23 15:30:19 -0500

    Capifying the app

---
+++ config/deploy.rb
@@ -0,0 +1,39 @@
+set :application, "helpdesk"
+set :domain, "www.nsnmwcaretelcel.com"
+
+##
+# Settings
+##
+#load 'ext/passenger-mod-rails.rb'  # Restart task for use with mod_rails
+set :deploy_to, "/var/rails/#{application}"
+default_run_options[:pty] = true
+set :use_sudo, true
+set :user, "deployer"
+set :runner, "root"
+
+##
+# Git
+##
+set :repository, "git@github.com:alexjr85/helpdesk.git"
+set :scm, :git
+set :branch, "master"
+set :deploy_via, :remote_cache
+set :git_enable_submodules, 1
+
+##
+# Servers
+##
+role :web, domain
+role :app, domain
+
+namespace :deploy do
+  desc "Restarting mod_rails with restart.txt"
+  task :restart, :roles => :app, :except => { :no_release => true } do
+    run "touch #{current_path}/tmp/restart.txt"
+  end
+  
+  [:start, :stop].each do |t|
+      desc "#{t} task is a no-op with mod_rails"
+      task t, :roles => :app do ; end
+  end
+end
--- public/javascripts/jquery.js
+++
@@ -1,1 +0,0 @@
-../../vendor/plugins/jrails/javascripts/jquery.js
