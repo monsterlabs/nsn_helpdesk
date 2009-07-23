49b1a1f94d151e0aab30f7d927ea3d3fefacc12e
Fernando Reyes Jiménez <fereyji@gmail.com>
2009-07-23 13:43:04 -0500

    Adding CaptureTime and DueDate methods for Ticket. Enhancements for Show view.

--- app/views/operator/tickets/record.rb
+++ app/views/operator/tickets/record.rb
@@ -11,7 +11,7 @@ class Views::Operator::Tickets::Record < Erector::RailsWidget
   def table_header
     thead :class => "ui-widget-header", :id => "listing-head" do
       tr do
-        %w(CaseID Subject Status ReportedBy OpenedBy Region Priority Actions).each  do |column|
+        %w(CaseID Subject Status ReportedBy Region AttendedBy Priority Actions).each  do |column|
           th { text column }
         end
       end
@@ -22,21 +22,14 @@ class Views::Operator::Tickets::Record < Erector::RailsWidget
     tbody do    
       @collection.each do |ticket|
         tr :id => ticket.dom_id do
-           td { rawtext ticket.case_id  }
-           td { rawtext ticket.failure.name }
-           td { rawtext ticket.status.name }
-          unless ticket.reported_by.nil?
-            td { rawtext ticket.reported_by.person.fullname } 
-            td { rawtext ticket.opened_by.person.fullname }
-            td { rawtext ticket.link.region.name}
-            else
-            td { label "" } 
-            td { label "Not opened"}
-            td { label ""}
-          end
-            td { rawtext ticket.priority.name}
-
-            td {
+          td { rawtext ticket.case_id  }
+          td { rawtext ticket.failure.name }
+          td { rawtext ticket.status.name }
+          td { rawtext ticket.reported_by.person.fullname } 
+          td { rawtext ticket.link.region.name}
+          td { rawtext ticket.attended_by.person.fullname } 
+          td { rawtext ticket.priority.name}
+          td {
               link_to 'Edit', :action => 'edit', :id => ticket.id 
               text ' | '
               link_to 'Show', :action => 'show', :id => ticket.id
