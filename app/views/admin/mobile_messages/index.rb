6c5966701dad5e64991c41ff957dc7739948640f
María Donají Velázquez Orduña <xunaxidonaji@gmail.com>
2009-07-27 23:09:08 -0500

    Fixing mobile_messages table error

--- app/views/admin/mobile_messages/index.rb
+++ app/views/admin/mobile_messages/index.rb
@@ -13,11 +13,13 @@ class Views::Admin::MobileMessages::Index < Views::Layouts::Application
   def table_body
     tbody do    
        @collection.each do |record|
-        td { rawtext record.ticket.case_id  }
-        td { rawtext record.phone_number }
-        td { rawtext record.body }
-        td { rawtext (record.status ? 'Sent' : 'Pending') }
-        td { rawtext record.created_at }
+         tr do
+           td { rawtext record.ticket.case_id  }
+           td { rawtext record.phone_number }
+           td { rawtext record.body }
+           td { rawtext (record.status ? 'Sent' : 'Pending') }
+           td { rawtext record.created_at }
+         end
       end
     end
     paginator @collection
