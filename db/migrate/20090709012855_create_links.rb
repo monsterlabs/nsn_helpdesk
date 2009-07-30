d3f739cf3ae1c40cf6147c63cc3dd07ed0f8b7fd
Alejandro Juarez <karmatr0n.dixit@gmail.com>
2009-07-30 00:23:16 -0500

    Moving frequency attributes to link tables

--- db/migrate/20090709012855_create_links.rb
+++ db/migrate/20090709012855_create_links.rb
@@ -1,9 +1,17 @@
 class CreateLinks < ActiveRecord::Migration
   def self.up
+    
+    create_table :time_zones do |t|
+      t.string :name      
+      t.timestamps
+    end
+    
     create_table :links do |t|
       t.references :region, :city
       t.text       :sites
-      t.string     :time_zone
+      t.text       :frequency_tx, :frequency_rx
+      t.string     :current_status # 0 + 0, 1 + 0., 1 + 1
+      t.references :time_zone
       t.text       :other
       t.timestamps
     end
