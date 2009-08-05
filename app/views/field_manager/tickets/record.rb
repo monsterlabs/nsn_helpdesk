b40057a1d9ad76953d3db080b5d9b9d7f114adb8
Fernando Reyes Jiménez <fereyji@gmail.com>
2009-08-05 01:01:37 -0500

    Enhacements for record views of Admin and FieldManager

--- app/views/field_manager/tickets/record.rb
+++ app/views/field_manager/tickets/record.rb
@@ -8,19 +8,10 @@ class Views::FieldManager::Tickets::Record < Erector::RailsWidget
         td { rawtext ticket.link.region.name}
         td { rawtext ticket.link.sites}          
         td { if ticket.affected_site.empty? then text "Both" else text ticket.affected_site end }
-        td { rawtext ticket.frequency_tx}          
-        td { rawtext ticket.frequency_rx}
-        td { rawtext ticket.priority.name}          
-        td { rawtext ticket.failure.name}
+        td { rawtext ticket.status.name}          
         td { rawtext ticket.alarm}
         td { rawtext ticket.reported_by.person.fullname } 
-        td { rawtext ticket.attended_by.person.fullname}
         td { rawtext ticket.opened_at}
-        td { if ticket.opened_by.person.nil? then text ticket.opened_by.login else text ticket.opened_by.person.fullname end}
-        td { rawtext ticket.due_date }
-        td { rawtext ticket.status.name }
-        ticket.status.id > 2 ? td { rawtext ticket.updated_at } : td { rawtext ""}
-        td { rawtext ticket.capture_time }
         td {
           link_to 'Edit', {:action => :edit, :id => ticket.id}, ui_style(:button)
           link_to 'Show', {:action => :show, :id => ticket.id}, ui_style(:button)
