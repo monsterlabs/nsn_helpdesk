6e03b8eda9613f61a3d0933d84af730bfe0a2055
Alejandro Juarez <karmatr0n.dixit@gmail.com>
2009-08-08 00:52:36 -0500

    Adding mobile_messages notifications into link_observer

--- app/models/link_observer.rb
+++ app/models/link_observer.rb
@@ -2,6 +2,7 @@ class LinkObserver < ActiveRecord::Observer
   observe :link
 
   def after_update(link)
+    MobileMessage.create(:messageable => link, :phone_number => link.emergency_phone_number)
     Notifier.deliver_link_notifications(link) #if link.modified_by.login != 'admin'
   end
 
