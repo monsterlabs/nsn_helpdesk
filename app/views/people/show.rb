a42fcb8f892ac2d4faf72e5b5d609d498d49c1a2
Alejandro Juarez <karmatr0n.dixit@gmail.com>
2009-08-08 00:07:37 -0500

    Adding show photo to views/people/show.rb

--- app/views/people/show.rb
+++ app/views/people/show.rb
@@ -2,33 +2,37 @@ class Views::People::Show < Views::Layouts::Application
   needs :person
   def content
     unless @person.nil?
-      h2 'Datos personales'
-      # div :class => 'alpha prefix_3 grid_1' do
-      #   if @person.photo.size == nil
-      #     rawtext image_tag "/photos/thumb/missing.png"
-      #   else
-      #     rawtext image_tag @person.photo.url(:thumb)
-      #   end
-      # end
-      table :id => "profile-info" do
-        tr do 
-          td { b 'Firstname:' }
-          td { rawtext @person.firstname }
-        end
+      div :class => 'grid_10' do
+        div :class => 'grid_4  alpha', :style => 'margin-left: -10px;' do
+          h2 'Datos personales'
+          table :id => "profile-info" do
+            tr do 
+              td { b 'Firstname:' }
+              td { rawtext @person.firstname }
+            end
 
-        tr do 
-          td { b 'Lastname:' }
-          td { rawtext @person.lastname }
-        end
+            tr do 
+              td { b 'Lastname:' }
+              td { rawtext @person.lastname }
+            end
 
-        tr do 
-          td { b 'Company:' }
-          td { rawtext @person.company.name  unless @person.company.nil? }
-        end
+            tr do 
+              td { b 'Company:' }
+              td { rawtext @person.company.name  unless @person.company.nil? }
+            end
 
-        tr do 
-          td { b 'Region:' }
-          td { rawtext @person.region.name unless @person.region.nil? }
+            tr do 
+              td { b 'Region:' }
+              td { rawtext @person.region.name unless @person.region.nil? }
+            end
+          end
+        end
+        div :class => 'grid_4 prefix_1 omega', :id => 'photo', :style => 'padding-top: 30px;' do
+          if @person.photo.size == nil
+            rawtext image_tag "/photos/thumb/missing.png"
+          else
+            rawtext image_tag @person.photo.url(:medium)
+          end                    
         end
       end
     end
