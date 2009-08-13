f951ad62cbdb0b5be8e622ff8ed9eae0e70014c6
Alejandro Juarez <karmatr0n.dixit@gmail.com>
2009-08-13 00:55:16 -0500

    Bug fix for products_controller, company and products associations and minor changes for super_scaffold plugin

--- app/controllers/admin/products_controller.rb
+++ app/controllers/admin/products_controller.rb
@@ -1,3 +1,3 @@
 class Admin::ProductsController < ApplicationController
-  super_scaffold :class_name => Product, :name_space => "admin", :columns => {:name=> 'string', :description => 'text', :url => 'string', :company_id => 'select'}
+  super_scaffold :class_name => Product, :name_space => "admin", :columns => {:name=> 'string', :description => 'text', :url => 'string', :company_id => 'select' }
 end
