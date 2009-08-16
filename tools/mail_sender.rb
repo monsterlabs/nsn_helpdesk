f97a07f8731c2759801208379be5e2c6ef60c42b
Alejandro Juarez <karmatr0n.dixit@gmail.com>
2009-08-16 18:13:27 -0500

    minor changes

--- tools/mail_sender.rb
+++ tools/mail_sender.rb
@@ -9,12 +9,13 @@ class MailDaemon < SimpleDaemon::Base
     puts "Loading mail accounts and mail server list.. " + Time.now.to_s
     file_path = "#{RAILS_ROOT}/config/mail_servers.yml"
     mail_servers = YAML.load_file(file_path).collect { |key, value| value.to_options! }
-    seconds = (1..30).to_a
+    seconds = (1..60).to_a
     puts "Starting Mail Sender at " + Time.now.to_s
     loop do
       if Mail.respond_to? :process
         ActionMailer::Base.smtp_settings = mail_servers[rand(mail_servers.size)]
         Mail.process(:limit => 15) 
+        Mail.update_all("locked = 't', entries = 0")
         puts "Sending email at " + Time.now.to_s
       end
       sleep(seconds[rand(seconds.size)])
