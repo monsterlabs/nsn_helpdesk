4fdfd35d9d022b8ce314c5a66baf0da2c1a4e570
Alejandro Juarez <karmatr0n.dixit@gmail.com>
2009-08-17 22:54:05 -0500

    Disabling SMS sending

--- app/models/ticket_observer.rb
+++ app/models/ticket_observer.rb
@@ -2,7 +2,7 @@ class TicketObserver < ActiveRecord::Observer
   observe :ticket
   def after_create(ticket)
     if ticket.reported_priority.name == 'High' and ENV['RAILS_ENV'] == 'production'
-      MobileMessage.create(:messageable => ticket, :phone_number => ticket.emergency_phone_number) 
+      #MobileMessage.create(:messageable => ticket, :phone_number => ticket.emergency_phone_number) 
     end
     Notifier.queue(:ticket_notifications, ticket)
   end
