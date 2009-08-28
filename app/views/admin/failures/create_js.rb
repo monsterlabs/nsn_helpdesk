7440b7519a3b9740044f5f6b16fdf5b7f62d6ca2
Juan Germán Castañeda Echevarría <juanger@gmail.com>
2009-08-27 19:47:55 -0500

    Fixing display of limited failure names when adding a failure in new ticket view.

--- app/views/admin/failures/create_js.rb
+++ app/views/admin/failures/create_js.rb
@@ -1,6 +1,6 @@
 class Views::Admin::Failures::CreateJs < Views::Layouts::ApplicationJs
   def js_content
     # I'm not using simple_select to make a custom selection on the newly created record
-    rawtext select(:ticket, :failure_id, options_from_collection_for_select(Failure.all, 'id', 'name', @record.id))
+    rawtext select(:ticket, :failure_id, options_from_collection_for_select(Failure.all, 'id', 'failure_length_limited', @record.id))
   end
 end
