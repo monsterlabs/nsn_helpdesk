7e479d296511d2380b6c5a24bf7d0c9f7b133934
Alejandro Juarez <karmatr0n.dixit@gmail.com>
2009-08-28 19:28:48 -0500

    Enabling sms sending to field_managers for corrective cases (low and medium), and minor changes

--- app/models/link_observer.rb
+++ app/models/link_observer.rb
@@ -2,13 +2,11 @@ class LinkObserver < ActiveRecord::Observer
   observe :link
 
   def after_update(link)
-    if ENV['RAILS_ENV'] == 'production'
-      User.field_managers.each do |fm|
-        fm.mobile_phones.each do |phone_number|
-          # MobileMessage.create(:messageable => link, :phone_number => phone_number)
-        end
+    link.region.users.field_managers.each do |fm|
+      fm.mobile_phones.each do |phone_number|
+        MobileMessage.create(:messageable => link, :phone_number => phone_number)
       end
     end
-    Notifier.queue(:link_notifications, link) if !link.modified_by.has_role?(:admin) and !link.modified_by.has_role?(:field_manager)
+    Notifier.queue(:link_notifications, link) 
   end
 end
--- app/models/ticket_observer.rb
+++ app/models/ticket_observer.rb
@@ -1,17 +1,23 @@
 class TicketObserver < ActiveRecord::Observer
   observe :ticket
   def after_create(ticket)
-    if ticket.reported_priority.name == 'High' and ENV['RAILS_ENV'] == 'production'
+    if ticket.reported_priority.name == 'High'
       User.emergencies.each do |emergency_user|
         emergency_user.mobile_phones.each do |phone_number|
+          MobileMessage.create(:messageable => ticket, :phone_number => phone_number) 
+        end
+      end
+    else
+      ticket.link.region.users.field_managers.each do |field_manager|
+        field_manager.mobile_phones.each do |phone_number|
           MobileMessage.create(:messageable => ticket, :phone_number => phone_number) 
         end
       end
     end
     Notifier.queue(:ticket_notifications, ticket)
   end
-  
+
   def after_update(ticket)
-    Notifier.queue(:ticket_closed_notification, ticket) if ticket.status.name == 'Closed' and ENV['RAILS_ENV'] == 'production'
+    Notifier.queue(:ticket_closed_notification, ticket) if ticket.status.name == 'Closed'
   end
 end
