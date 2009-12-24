f03a0cce51c542191329bcf6114b72d2d8d68a8c
Alejandro Juarez <karmatr0n.dixit@gmail.com>
2009-12-24 11:00:17 -0600

    Cleanning duplication calls to current_user_session

--- lib/authentication_system.rb
+++ lib/authentication_system.rb
@@ -6,7 +6,7 @@ module AuthenticationSystem
 
   def current_user
     return @current_user if defined?(@current_user)
-    @current_user = current_user_session && current_user_session.record
+    @current_user = current_user_session.record
   end
  
   def require_user
