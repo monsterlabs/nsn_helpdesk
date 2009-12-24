bbc742d1732aec1266796fb0694c4f0975df066a
Alejandro Juarez <karmatr0n.dixit@gmail.com>
2009-12-23 23:12:51 -0600

    Fixing bugs with the scheduler based on whenever gem and sftp_client

--- config/schedule.rb
+++ config/schedule.rb
@@ -1,7 +1,6 @@
 #set :output, "#{RAILS_ROOT}/log/db_backup_cron_log.log"
 every 1.day, :at => '11:59 pm' do
-    dump_file = Time.now.strftime("%Y%m%d%H%M%S") + '_nsn_casetracker.pg_dump'
-    local_file = File.join(RAILS_ROOT, 'tmp', dump_file)
+    local_file = File.join(RAILS_ROOT, 'tmp', 'nsn_casetracker.pg_dump')
     command "pg_dump -Ft -b -U sosbox sosbox > #{local_file}"
     command "ruby #{RAILS_ROOT}/tools/sftp_client.rb #{local_file}"
     command "rm #{local_file}"
--- tools/sftp_client.rb
+++ tools/sftp_client.rb
@@ -2,10 +2,10 @@ require 'rubygems'
 require 'net/sftp'
 local_file = ARGV[0]
 if File.exist? local_file
-  filename = File.basename(local_file)
+  filename =  File.basename(local_file)
   Net::SFTP.start('lattice.com.mx', 'alex', :password => 'xela2oo9$') do |sftp|
     remote_dir = "/var/alex/backups/nsn/casetracker"
-    remote_file = File.join(remote_dir, filename)
+    remote_file = File.join(remote_dir, "#{Time.now.strftime('%Y%m%d%H%M%S')}_#{filename}")
     sftp.upload! local_file, remote_file
   end
 end
