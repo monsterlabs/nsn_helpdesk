31156be7131cf52fef4d1ad09dd7ca7e93330449
Alejandro Juarez <karmatr0n.dixit@gmail.com>
2009-12-28 23:48:49 -0600

    Refactorin sftp_client

--- tools/sftp_client.rb
+++ tools/sftp_client.rb
@@ -4,34 +4,29 @@ require 'net/sftp'
 require 'date'
 require 'yaml'
 
-module SftpClient
-  def load_config
+class SftpClient
+  def initialize
+    @datetime = Time.now
     @config = YAML.load_file(File.join(File.dirname(__FILE__), '../config/sftp.yml'))
-  end
-
-  def sftp_connection
-    load_config
     @sftp = Net::SFTP.start(@config['host'], @config['username'], :password => @config['password']) 
   end
 
-  def upload_file(local_file, datetime)
+  def upload_file(local_file)
     if File.exist? local_file
-      sftp_connection.upload! local_file, File.join(@config['remote_dir'], "#{datetime.strftime('%Y%m%d%H%M%S')}_#{File.basename(local_file)}")
+      @sftp.upload! local_file, File.join(@config['remote_dir'], "#{@datetime.strftime('%Y%m%d%H%M%S')}_#{File.basename(local_file)}")
       File.delete local_file
     end
   end
 
-  def remove_remote_files(datetime)
-    sftp_connection
+  def remove_old_files(regexp)
     remote_dir = @config['remote_dir']
-    @sftp.dir.glob(remote_dir,'*.pg_dump*') do |entry|
+    @sftp.dir.glob(remote_dir,regexp) do |entry|
       remote_file = entry.name
       @sftp.remove! File.join(remote_dir, remote_file) if DateTime.parse(remote_file.split('_').first) < eval(@config['time_limit'])
     end
   end
 end
 
-include SftpClient
-current_datetime = Time.now
-SftpClient.upload_file ARGV[0], current_datetime
-SftpClient.remove_remote_files current_datetime
+@ftp_client = SftpClient.new
+@ftp_client.upload_file(ARGV[0])
+@ftp_client.remove_old_files('*.pg_dump*')
