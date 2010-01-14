32a6044ea99580ba28f12b1c9f5c6c69a65ee6f6
Alejandro Juarez <karmatr0n.dixit@gmail.com>
2010-01-14 16:46:47 -0600

    Changing time to make the database dump and to upload It

--- config/schedule.rb
+++ config/schedule.rb
@@ -1,7 +1,11 @@
 #set :output, "#{RAILS_ROOT}/log/db_backup_cron_log.log"
+every 1.day, :at => '11:55 pm' do
+    local_file = File.join(RAILS_ROOT, 'tmp', 'nsn_casetracker.pg_dump')
+    command "pg_dump -Ft -b -U sosbox sosbox > #{local_file}"
+end
+    
 every 1.day, :at => '11:59 pm' do
     local_file = File.join(RAILS_ROOT, 'tmp', 'nsn_casetracker.pg_dump')
     command "pg_dump -Ft -b -U sosbox sosbox > #{local_file}"
-    command "ruby #{RAILS_ROOT}/tools/sftp_client.rb #{local_file}"
 end
     
